0456d7aba334b65b2746fc77f1184ee92d2c1786
Xian Chen Ruan <a01006350@imac-9.ad.bcit.ca>
2018-11-23 11:29:17 -0800

    Initial Commit

---
+++ YummlyAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YummlyAPI
+//
+//  Created by Xian Chen Ruan on 2018-11-23.
+//  Copyright © 2018 Xian Chen Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
